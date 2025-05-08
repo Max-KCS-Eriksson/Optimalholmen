f3c014cd29e4933efd9cfaf8ff6ba27a22d9313b
Max Eriksson <teratoscincus@hotmail.com>
2025-05-08 11:34:56 +0200

    Init Spring Boot project

---
+++ src/main/java/com/maxeriksson/optimalholmen/OptimalholmenApplication.java
@@ -0,0 +1,13 @@
+package com.maxeriksson.optimalholmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimalholmenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimalholmenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxeriksson/optimalholmen/OptimalholmenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxeriksson.optimalholmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalholmenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
