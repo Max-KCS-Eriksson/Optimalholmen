9ad289ee42f9ba020de4bfd2f4d67f1ca5b987ca
Max Eriksson <teratoscincus@hotmail.com>
2025-05-09 14:29:39 +0200

    Create client for SMHI forecast service

---
+++ src/main/java/com/maxeriksson/optimalholmen/client/smhi/SmhiClient.java
@@ -0,0 +1,32 @@
+package com.maxeriksson.optimalholmen.client.smhi;
+
+import com.maxeriksson.optimalholmen.client.smhi.model.Smhi;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class SmhiClient {
+
+    WebClient webClient;
+
+    public SmhiClient() {
+        String baseUrl = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2";
+        webClient = WebClient.create(baseUrl);
+    }
+
+    public Mono<Smhi> getForecast(double longitude, double latitude) {
+        return webClient
+                .get()
+                .uri(
+                        uriBuilder ->
+                                uriBuilder
+                                        .path(
+                                                "/geotype/point/lon/{longitude}/lat/{latitude}/data.json")
+                                        .build(longitude, latitude))
+                .retrieve()
+                .bodyToMono(Smhi.class);
+    }
+}
